108dad9ff52e6aad60582e5b6a414b94b786783f
Allan <allanrodriguest@hotmail.com>
2023-02-25 19:28:52 -0500

    delete end

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express();
+const app = express()
 const MongoClient = require('mongodb').MongoClient
 const connectionString =
   'mongodb+srv://admin:admin123@cluster0.nzuywvc.mongodb.net/?retryWrites=true&w=majority'
@@ -10,35 +10,68 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     console.log('Connected to Database')
     const db = client.db('star-wars-quotes')
     const quotesCollection = db.collection('quotes')
-      app.use(express.static('public'))
-      app.use(bodyParser.urlencoded({ extended: true }))
-      app.use(bodyParser.json())
-      app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.set('view engine', 'ejs')
 
-      app.get('/', (req, res) => {
-        db.collection('quotes').find().toArray()
+    app.get('/', (req, res) => {
+      db.collection('quotes')
+        .find()
+        .toArray()
         .then(results => {
-          res.render('index.ejs', {quotes: results})
+          res.render('index.ejs', { quotes: results })
         })
         .catch(err => console.error(err))
-        
-        
-      })
+    })
 
-      app.post('/quotes', (req, res) => {
-        quotesCollection.insertOne(req.body)
+    app.post('/quotes', (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
         .then(result => {
           res.redirect('/')
         })
         .catch(error => console.error(error))
-      })
+    })
 
-      app.put('/quotes', (req,res) => {
-        console.log(req.body);
-      })
+    app.put('/quotes', (req, res) => {
+      quotesCollection
+        .findOneAndUpdate(
+          { name: 'Yoda' },
+          {
+            $set: {
+              name: req.body.name,
+              quote: req.body.quote
+            }
+          },
+          {
+            upsert: true
+          }
+        )
+        .then(res => {
+          res.json('Success')
+          window.location.reload(true)
+        })
+        .catch(error => console.error(error))
+    })
 
-      app.listen(3000, () => {
-        console.log('Listening on port: 3000')
-      })
+    app.delete('/quotes', (req, res) => {
+      quotesCollection
+        .deleteOne({
+          name: req.body.name
+        })
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('No quote do delete')
+          }
+
+          res.json(`Deleted Darth Vader's quote`)
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.listen(3000, () => {
+      console.log('Listening on port: 3000')
+    })
   })
   .catch(error => console.error(error))
